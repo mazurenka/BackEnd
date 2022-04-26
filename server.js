69d0fe5aab6e6780f70a95275335ba72e2dcbe31
mazurenka <an.drej@mail.ru>
2022-04-26 10:31:22 +0200

    5

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require('http')
 
+let requestsCount = 0
+
 const server = http.createServer( (request, response) => {
-    response.write('hey yo!')
+
+    requestsCount++
+
+    switch (request.url) {
+        case '/students':
+            response.write('names')
+            break
+        case '/courses':
+            response.write('yoyoyo')
+            break
+        default:
+            response.write('not found 404')
+    }
+
+    response.write('hey yo!' + requestsCount)
     response.end()
 } )
 
